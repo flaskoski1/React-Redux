8bf25875a8b3c725a23177b786f823479fff7512
Filip Lashkoski <flaskoski@gmail.com>
2020-10-05 12:02:43 +0200

    finish

--- src/sagas/saga.js
+++ src/sagas/saga.js
@@ -1,7 +1,7 @@
 import { takeLatest, takeEvery, put } from "redux-saga/effects";
 import { delay } from "redux-saga/effects";
 function* ageUpAsync() {
-  yield delay(4000);
+  yield delay(3000);
   yield put({ type: "AGE_UP_ASYNC", value: 1 });
 }
 
